bc31c9d9086775cfd344531fef3db873f6c12b11
Matthew Britt <mwbritt@hotmail.com>
2019-12-28 05:13:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,11 +27,11 @@ def monopoly_with_third_tier
                   :four_pieces_owned => 200
                   },
                  :names =>
-                  :reading_railroad => {},
+                 {:reading_railroad => {},
                   :pennsylvania_railroad => {},
                   :b_and_o_railroad => {},
-                  :
-                 {
+                  :shortline_railroad => {}
+                  }
                 }
     
   }
