07f38c8c4d2ccfe64c9d00ef1be013f0ad48d06f
Matthew Britt <mwbritt@hotmail.com>
2019-12-28 04:54:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,10 +12,10 @@ def monopoly_with_second_tier
   base_hash = {:railroads 
                 => {:pieces => 4, 
                     :rent_in_dollars => 
-                    {}
-                    {}
-                    {}
-                    {}
+                    {},
+                    {},
+                    {},
+                    {},
                 }
               }
 end
