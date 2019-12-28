f453fa112c53fb0b2d7115ce7f580f2bd8f29c54
Matthew Britt <mwbritt@hotmail.com>
2019-12-28 04:58:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,15 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base_hash = {:railroads =>
-                {:pieces => 4,}}
+                {:pieces => 4,
+                 :rent_in_dollars =>
+                 {},
+                 {},
+                 {}
+                 {}
+                }
+    
+  }
 end
 
 def monopoly_with_fourth_tier
